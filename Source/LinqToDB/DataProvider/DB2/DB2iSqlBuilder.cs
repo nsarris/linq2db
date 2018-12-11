7d23346f64c84522c21d2597831f0a4a39e5d452
Nikos Sarris <nsarris@gmail.com>
2018-12-11 18:08:36 +0200

    Fixed reverese files

--- Source/LinqToDB/DataProvider/DB2/DB2iSqlBuilder.cs
+++ Source/LinqToDB/DataProvider/DB2/DB2iSqlBuilder.cs
@@ -4,21 +4,21 @@ namespace LinqToDB.DataProvider.DB2
 {
 	using SqlProvider;
 
-	class DB2zOSSqlBuilder : DB2SqlBuilderBase
+	class DB2iSqlBuilder : DB2SqlBuilderBase
 	{
-		public DB2zOSSqlBuilder(ISqlOptimizer sqlOptimizer, SqlProviderFlags sqlProviderFlags, ValueToSqlConverter valueToSqlConverter)
+		public DB2iSqlBuilder(ISqlOptimizer sqlOptimizer, SqlProviderFlags sqlProviderFlags, ValueToSqlConverter valueToSqlConverter)
 			: base(sqlOptimizer, sqlProviderFlags, valueToSqlConverter)
 		{
 		}
 
 		protected override ISqlBuilder CreateSqlBuilder()
 		{
-			return new DB2zOSSqlBuilder(SqlOptimizer, SqlProviderFlags, ValueToSqlConverter);
+			return new DB2iSqlBuilder(SqlOptimizer, SqlProviderFlags, ValueToSqlConverter);
 		}
 
 		protected override DB2Version Version
 		{
-			get { return DB2Version.zOS; }
+			get { return DB2Version.i; }
 		}
 	}
 }
--- Source/LinqToDB/DataProvider/DB2/DB2zOSSqlBuilder.cs
+++ Source/LinqToDB/DataProvider/DB2/DB2zOSSqlBuilder.cs
@@ -4,21 +4,21 @@ namespace LinqToDB.DataProvider.DB2
 {
 	using SqlProvider;
 
-	class DB2iSqlBuilder : DB2SqlBuilderBase
+	class DB2zOSSqlBuilder : DB2SqlBuilderBase
 	{
-		public DB2iSqlBuilder(ISqlOptimizer sqlOptimizer, SqlProviderFlags sqlProviderFlags, ValueToSqlConverter valueToSqlConverter)
+		public DB2zOSSqlBuilder(ISqlOptimizer sqlOptimizer, SqlProviderFlags sqlProviderFlags, ValueToSqlConverter valueToSqlConverter)
 			: base(sqlOptimizer, sqlProviderFlags, valueToSqlConverter)
 		{
 		}
 
 		protected override ISqlBuilder CreateSqlBuilder()
 		{
-			return new DB2iSqlBuilder(SqlOptimizer, SqlProviderFlags, ValueToSqlConverter);
+			return new DB2zOSSqlBuilder(SqlOptimizer, SqlProviderFlags, ValueToSqlConverter);
 		}
 
 		protected override DB2Version Version
 		{
-			get { return DB2Version.i; }
+			get { return DB2Version.zOS; }
 		}
 	}
 }
